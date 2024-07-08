8937fa30422ffc8db71eb2ce992a831defe95210
SKRTEEEEEE <adanreh.m@gmail.com>
2024-07-08 21:25:26 +0200

    build(app): Working todo-debounce example

--- callback/app/src/app.ts
+++ callback/app/src/app.ts
@@ -1,25 +1,50 @@
-// Cargar los módulos
-import express from 'express';
-import bodyParser from "body-parser"
-//Importar archivos
-import rutas from "./rutas";
-
-const app = express();
-const PORT = process.env.PORT || 4001;
-
-// app.set('view engine', 'ejs');
-// app.set("views", __dirname + "/views") 
+// Funciones App To Do List
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(express.static('public'));
-app.use(rutas);
-
-
-
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto: http://localhost:${PORT}`);
-});
-
+export interface Task {
+    id: number;
+    description: string;
+    completed: boolean;
+  }
+  
+  export let tasks: Task[] = [];
+  
+  // Función para verificar si un ID ya existe
+  const idExists = (id: number): boolean => {
+    return tasks.some(task => task.id === id);
+  }
+  // Generador de ID único
+  const generateUniqueId = (): number => {
+    let id: number = 1;
+    while(idExists(id)){
+        id++
+    }
+    return id;
+  }
+  
+  export const addTask = (description: string): void => {
+    if (tasks.length >= 99) {
+        console.log('Número máximo de tareas alcanzado');
+        return;
+    }
+    const newTask: Task = {
+        id: generateUniqueId(),
+        description,
+        completed: false
+    };
+  
+    tasks.push(newTask);
+  };
+  export const listTasks = (): Task[] => {
+    return tasks; // Devuelve el array de tareas
+  };
+  export const completeTask = (id: number): void => {
+    const task = tasks.find(task => task.id === id);
+    if (task) {
+        task.completed = true;
+    }
+  };
+  export const deleteTask = (id: number): void => {
+    tasks = tasks.filter(task => task.id !== id);
+  };
+  
--- callback/app/src/rutas.ts
+++ callback/app/src/rutas.ts
@@ -1,11 +1,32 @@
 import express from "express"
+import path from "path"
+import { debouncedAddTask, debouncedCompleteTask, debouncedDeleteTask, handleGetTasks } from "./handle";
 
 const rutas = express.Router();
+
+// Obtener la ruta absoluta al directorio 'public'
+const publicDirectory = path.resolve('public');
 
 rutas.get('/',(req,res)=>{
     res.sendFile('index.html');
 })
 
+rutas.get("/todo-debounce",(req,res)=>{
+    res.sendFile('todo-debounce.html',  { root: publicDirectory });
+})
+rutas.put("/tasks/:id/complete", (req, res)=>{
+    debouncedCompleteTask(req, res)
+})
+rutas.delete("/tasks/:id", (req, res)=>{
+    debouncedDeleteTask(req, res)
+}) 
+rutas.post("/tasks/", (req, res)=>{
+    debouncedAddTask(req, res)
+})
 
+rutas.get("/tasks/", (req, res)=>{
+    handleGetTasks(req, res)
+
+})
 
 export default rutas;
